924aa0cef424967c295279ce3ca328c4503b832a
EK791 <ek.fullone@gmail.com>
2021-12-23 15:00:54 +0100

    esercizioJS

---
+++ Dipendenti.js
@@ -0,0 +1,12 @@
+JSON> presenze [
+    {"anno": 2021, "numGiorno": 1, "idDipendente": 0, "orelav": 5},
+    {"anno": 2021, "numGiorno": 2, "idDipendente": 0, "orelav": 8},
+    {"anno": 2021, "numGiorno": 10, "idDipendente": 0, "orelav": 1},
+    {"anno": 2021, "numGiorno": 20, "idDipendente": 0, "orelav": 7},
+    {"anno": 2021, "numGiorno": 1, "idDipendente": 1, "orelav": 5},
+    {"anno": 2021, "numGiorno": 7, "idDipendente": 1, "orelav": 8},
+    {"anno": 2021, "numGiorno": 10, "idDipendente": 2, "orelav": 1},
+    {"anno": 2021, "numGiorno": 20, "idDipendente": 4, "orelav": 7}
+    ]
+
+    console.log (presenze[0].orelav)
